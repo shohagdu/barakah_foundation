5a6c55b54b72c647a89fa8646a294f1fcf985b98
Omar <omarshohag93@gmail.com>
2026-05-16 23:49:55 +0600

    update files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
+// Use Vite's base URL so routes work in subfolder deploys (e.g. /barakah_foundation/)
+const basename = import.meta.env.BASE_URL.replace(/\/$/, '')
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
